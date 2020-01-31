3add398a16aa761b31298c8cb8da2fbab336b1e2
EpicKnarvik97 <kristian.knarvik@knett.no>
2020-01-31 13:18:30 +0100

    Legger til manglende metoder i IDrawableGame

--- src/main/java/inf112/skeleton/app/IDrawableGame.java
+++ src/main/java/inf112/skeleton/app/IDrawableGame.java
@@ -8,6 +8,18 @@ import java.util.List;
 public interface IDrawableGame {
 
     /**
+     * Gets the screen width of the game
+     * @return A positive integer
+     */
+    int getWidth();
+
+    /**
+     * Gets the screen height of the game
+     * @return A positive integer
+     */
+    int getHeight();
+
+    /**
      * Gets a list of objects which are to be rendered
      * @return A list of drawable objects in the order they are to be drawn
      */

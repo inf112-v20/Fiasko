5367ab1c42b9be2c62f818812ca5b0857efb390b
Torbjørn Lunde Jensen <tje012@uib.no>
2020-02-20 11:14:20 +0100

    Adds testing to Tile class

---
+++ src/test/java/inf112/fiasko/roborally/TileTest.java
@@ -0,0 +1,41 @@
+package inf112.fiasko.roborally;
+
+import inf112.fiasko.roborally.element_properties.Direction;
+import inf112.fiasko.roborally.element_properties.TileType;
+import inf112.fiasko.roborally.objects.Tile;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TileTest {
+    private Tile tile;
+    private Tile tile2;
+
+    @Before
+    public void setUp() {
+        tile = new Tile(TileType.HOLE, Direction.NORTH);
+        tile2 = new Tile(TileType.COGWHEEL_RIGHT, Direction.SOUTH);
+    }
+
+    @Test
+    public void getTileTypeFromTile() {
+        assertEquals(TileType.HOLE, tile.getTileType());
+    }
+
+    @Test
+    public void getTileTypeFromTile2() {
+        assertEquals(TileType.COGWHEEL_RIGHT, tile2.getTileType());
+    }
+
+
+    @Test
+    public void getDirectionFromTile() {
+        assertEquals(Direction.NORTH, tile.getDirection());
+    }
+
+    @Test
+    public void getDirectionFromTile2() {
+        assertEquals(Direction.SOUTH, tile2.getDirection());
+    }
+}

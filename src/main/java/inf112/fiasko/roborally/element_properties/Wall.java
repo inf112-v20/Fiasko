0ee705252b145e463661d7e96224011ce0ce7cf3
Petter Tobias Madsen <tobe-madsen@hotmail.no>
2020-02-20 10:59:11 +0100

    Tobias og Gabriel added comments to the wall class

--- src/main/java/inf112/fiasko/roborally/element_properties/Wall.java
+++ src/main/java/inf112/fiasko/roborally/element_properties/Wall.java
@@ -1,18 +1,34 @@
 package inf112.fiasko.roborally.element_properties;
 
 public class Wall {
+    /**
+     * This class is a representation of a wall
+     */
     private WallType wall;
     private Direction direction;
 
+    /**
+     * Initializes the wall
+     * @param wall gives the type of wall eks. wall normal or wall corner
+     * @param direction gives the direction the wall is facing.
+     */
     public Wall (WallType wall,Direction direction){
         this.wall = wall;
         this.direction = direction;
     }
 
+    /**
+     * Gets the type of the wall
+     * @return the wall type
+     */
     public WallType getWallType() {
         return wall;
     }
 
+    /**
+     * Gets the direction of the wall
+     * @return the direction of the wall
+     */
     public Direction getDirection(){
         return direction;
     }

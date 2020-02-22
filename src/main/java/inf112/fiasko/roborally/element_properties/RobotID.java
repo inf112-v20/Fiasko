248b842536decb72ee55b3833a7f04fa91b556f7
EpicKnarvik97 <kristian.knarvik@knett.no>
2020-02-22 23:12:51 +0100

    Legger til en enum for ulike roboter

---
+++ src/main/java/inf112/fiasko/roborally/element_properties/RobotID.java
@@ -0,0 +1,15 @@
+package inf112.fiasko.roborally.element_properties;
+
+/**
+ * This class represents an id for marking specific robots
+ */
+public enum RobotID {
+    ROBOT_1,
+    ROBOT_2,
+    ROBOT_3,
+    ROBOT_4,
+    ROBOT_5,
+    ROBOT_6,
+    ROBOT_7,
+    ROBOT_8
+}

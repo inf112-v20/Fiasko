6f298797e771b35865a3a56e6033cd5496a56f57
EpicKnarvik97 <kristian.knarvik@knett.no>
2020-02-25 16:58:59 +0100

    Øker størrelsen på vist brett

--- src/main/java/inf112/fiasko/roborally/Main.java
+++ src/main/java/inf112/fiasko/roborally/Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "Game Board";
-        cfg.width = 768;
-        cfg.height = 769;
+        cfg.width = 900;
+        cfg.height = 900;
 
         new LwjglApplication(new GameLauncher(), cfg);
     }

726efa3cb43fef045d7249c54dc8af3c631769b6
Steinar Aalstad Lillesund <Steinar.Lillesund@Student.uib.no>
2020-02-27 12:19:18 +0100

    Made tests for the position.equals method.

--- src/test/java/inf112/fiasko/roborally/element_properties/PositionTest.java
+++ src/test/java/inf112/fiasko/roborally/element_properties/PositionTest.java
@@ -1,23 +1,35 @@
 package inf112.fiasko.roborally.element_properties;
 
-import inf112.fiasko.roborally.element_properties.Position;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class PositionTest {
-    private Position testPosition;
+    private Position testPosition1;
+    private Position testPosition2;
+    private Position testPosition3;
+    private Position testPosition4;
     @Before
     public void setUp() {
-        testPosition = new Position(3, 4);
+        testPosition1 = new Position(3, 4);
+        testPosition2 = new Position(2, 3);
+        testPosition3 = new Position(3, 4);
+        testPosition4 = new Position(3, 3);
     }
     @Test
     public void testGetXPosition(){
-        assertEquals(3,testPosition.getXCoordinate());
+        assertEquals(3,testPosition1.getXCoordinate());
     }
+
     @Test
     public void testGetYPosition(){
-        assertEquals(4,testPosition.getYCoordinate());
+        assertEquals(4,testPosition1.getYCoordinate());
     }
+
+    @Test
+    public void testSamePositionsAreEqual() { assertEquals(testPosition1, testPosition3);}
+
+    @Test
+    public void testDifferentPositionsAreNotEqual() { assertNotEquals(testPosition2, testPosition4);}
 }

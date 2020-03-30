b51207584204c27493350283b481ccc5911f5875
EpicKnarvik97 <kristian.knarvik@knett.no>
2020-03-30 14:07:53 +0200

    Revert "Fjernet ubrukt kode"
    Legger tilbake kode fjernet under opprydding
    This reverts commit d85d4e936aa04afde371e610dc3c546d50b966de.

---
+++ src/main/java/inf112/fiasko/roborally/game_wrapper/SimpleButton.java
@@ -0,0 +1,37 @@
+package inf112.fiasko.roborally.game_wrapper;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
+/**
+ * This class generates a simple text button using a default skin
+ */
+public class SimpleButton {
+    private final TextButton button;
+
+    /**
+     * Instantiates a new simple button
+     * @param text The text to display on the button
+     * @param font The font to use to draw button text
+     */
+    public SimpleButton(String text, BitmapFont font) {
+        TextureAtlas buttonAtlas = new TextureAtlas(Gdx.files.internal("uiskin.atlas"));
+        Skin skin = new Skin(buttonAtlas);
+        TextButton.TextButtonStyle confirmCardsStyle = new TextButton.TextButtonStyle();
+        confirmCardsStyle.font = font;
+        confirmCardsStyle.up = skin.getDrawable("default-round");
+        confirmCardsStyle.down = skin.getDrawable("default-round-down");
+        this.button = new TextButton(text, confirmCardsStyle);
+    }
+
+    /**
+     * Gets the button generated
+     * @return A button
+     */
+    public TextButton getButton() {
+        return this.button;
+    }
+}

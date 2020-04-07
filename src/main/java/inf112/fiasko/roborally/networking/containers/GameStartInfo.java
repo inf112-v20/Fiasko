cad7677bc6e8a784d48fcbeb47fa34b44859d5ef
EpicKnarvik97 <kristian.knarvik@knett.no>
2020-04-07 21:50:00 +0200

    Legger til manglende kommentarer for GameStartInfo

--- src/main/java/inf112/fiasko/roborally/networking/containers/GameStartInfo.java
+++ src/main/java/inf112/fiasko/roborally/networking/containers/GameStartInfo.java
@@ -4,28 +4,58 @@ import inf112.fiasko.roborally.objects.Player;
 
 import java.util.List;
 
+/**
+ * This class contains information about the game board to be used and the game's players
+ */
 public class GameStartInfo {
-    private String boardname;
-    private List<Player> playerlist;
-    public GameStartInfo(){}
+    private String boardName;
+    private List<Player> playerList;
 
-    public void setBoardname(String boardname) {
-        this.boardname = boardname;
+    /**
+     * Empty initialization method used by kryo
+     */
+    public GameStartInfo() {}
+
+    /**
+     * Sets the name of the board to be used
+     * @param boardName The name of the board to be used, with extension
+     */
+    public void setBoardName(String boardName) {
+        this.boardName = boardName;
     }
 
-    public void setPlayerlist(List<Player> playerlist) {
-        this.playerlist = playerlist;
+    /**
+     * Sets the list of players for the game
+     * @param playerList List of players for the game
+     */
+    public void setPlayerList(List<Player> playerList) {
+        this.playerList = playerList;
     }
 
-    public GameStartInfo(String boardname, List<Player> playerlist){
-        this.boardname=boardname;
-        this.playerlist=playerlist;
+    /**
+     * Instantiates a new GameStartInfo object
+     * @param boardName The name of the board to be used, with extension
+     * @param playerList List of players for the game
+     */
+    public GameStartInfo(String boardName, List<Player> playerList) {
+        this.boardName = boardName;
+        this.playerList = playerList;
     }
-    public List<Player> getPlayerlist(){
-        return playerlist;
+
+    /**
+     * Gets the list of players
+     * @return A list of players
+     */
+    public List<Player> getPlayerList() {
+        return playerList;
     }
-    public String getBoardname(){
-        return boardname;
+
+    /**
+     * Gets the board name
+     * @return The board name
+     */
+    public String getBoardName() {
+        return boardName;
     }
 
 }

c4c37ca4dd08214bb17d28b6fb235edb883c2aea
EpicKnarvik97 <kristian.knarvik@knett.no>
2020-04-07 15:15:06 +0200

    Fjerner kryo registrering av debug klasser

--- src/main/java/inf112/fiasko/roborally/utility/NetworkUtil.java
+++ src/main/java/inf112/fiasko/roborally/utility/NetworkUtil.java
@@ -2,10 +2,8 @@ package inf112.fiasko.roborally.utility;
 
 import com.esotericsoftware.kryo.Kryo;
 import inf112.fiasko.roborally.element_properties.RobotID;
-import inf112.fiasko.roborally.networking.ErrorResponse;
-import inf112.fiasko.roborally.networking.GameStartInfo;
-import inf112.fiasko.roborally.networking.SomeRequest;
-import inf112.fiasko.roborally.networking.SomeResponse;
+import inf112.fiasko.roborally.networking.containers.ErrorResponse;
+import inf112.fiasko.roborally.networking.containers.GameStartInfo;
 import inf112.fiasko.roborally.objects.IDeck;
 import inf112.fiasko.roborally.objects.Player;
 import inf112.fiasko.roborally.objects.ProgrammingCard;
@@ -19,8 +17,6 @@ public final class NetworkUtil {
      * @param kryo The kryo object to register the classes to
      */
     public static void registerClasses(Kryo kryo) {
-        kryo.register(SomeRequest.class);
-        kryo.register(SomeResponse.class);
         kryo.register(ErrorResponse.class);
         kryo.register(IDeck.class);
         kryo.register(ProgrammingCard.class);

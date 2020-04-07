1c47b627aff63d84f2450e12be56dc03eb1cb773
EpicKnarvik97 <kristian.knarvik@knett.no>
2020-04-07 15:16:23 +0200

    Legger til manglende mellomrom mellom paranteser og krøllparanteser

--- src/test/java/inf112/fiasko/roborally/objects/WallTest.java
+++ src/test/java/inf112/fiasko/roborally/objects/WallTest.java
@@ -7,27 +7,27 @@ import org.junit.Test;
 
 public class WallTest {
     @Test
-    public void testWallGetWallTypeNormal(){
+    public void testWallGetWallTypeNormal() {
         Wall testGetWall = new Wall(WallType.WALL_NORMAL, Direction.NORTH);
         assertEquals(WallType.WALL_NORMAL, testGetWall.getWallType());
     }
     @Test
-    public void testWallGetWallTypeCorner(){
+    public void testWallGetWallTypeCorner() {
         Wall testGetWall = new Wall(WallType.WALL_CORNER, Direction.NORTH);
         assertEquals(WallType.WALL_CORNER, testGetWall.getWallType());
     }
     @Test
-    public void testWallGetWallTypeLaserSingle(){
+    public void testWallGetWallTypeLaserSingle() {
         Wall testGetWall = new Wall(WallType.WALL_LASER_SINGLE, Direction.NORTH);
         assertEquals(WallType.WALL_LASER_SINGLE, testGetWall.getWallType());
     }
     @Test
-    public void testWallGetDirectionNorth(){
+    public void testWallGetDirectionNorth() {
         Wall testGetWall = new Wall(WallType.WALL_CORNER, Direction.NORTH);
         assertEquals(Direction.NORTH, testGetWall.getDirection());
     }
     @Test
-    public void testWallGetDirectionEast(){
+    public void testWallGetDirectionEast() {
         Wall testGetWall = new Wall(WallType.WALL_CORNER, Direction.EAST);
         assertEquals(Direction.EAST, testGetWall.getDirection());
     }

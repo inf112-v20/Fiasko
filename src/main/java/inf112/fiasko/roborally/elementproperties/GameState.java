f8fdc645533ac450fd1e075db4255fb87e7cc313
EpicKnarvik97 <kristian.knarvik@knett.no>
2020-04-17 12:52:28 +0200

    Fjerner ubrukte tilstander fra GameState

--- src/main/java/inf112/fiasko/roborally/elementproperties/GameState.java
+++ src/main/java/inf112/fiasko/roborally/elementproperties/GameState.java
@@ -1,16 +1,16 @@
 package inf112.fiasko.roborally.elementproperties;
 
+/**
+ * This enum represents the different states the game can be in
+ */
 public enum GameState {
-    //Indicates that the server is waiting for something
-    WAITING,
+
     //Gives the game Access to the client and server
     BEGINNING_OF_GAME,
     //Indicates that the users' input is being run. The board should be shown
     RUNNING_PROGRAMS,
     //Indicates that
     INITIAL_SETUP,
-    //
-    TURN_SETUP,
     //Indicates that the game is in the process of cleaning up after a turn
     TURN_CLEANUP,
     //Indicates that the user is in the process of choosing cards
@@ -19,13 +19,9 @@ public enum GameState {
     CHOOSING_POWER_DOWN,
     //Indicates that the user is in the process of choosing whether to stay in power down
     CHOOSING_STAY_IN_POWER_DOWN,
-    //Indicates that the user is in the process of sending their cards to the server
-    SENDING_CARDS,
     //Indicates that the game is won by a player
     GAME_IS_WON,
-
-    JUST_BEFORE_CHOOSING_CARDS,
-
+    //Indicates that the game is currently waiting for something
     LOADING
 
 }
